6aa13baf965a455d7e6140df9d5d5c336e9415bc
Pavan Choudhary <choudhary.pavan18@gmail.com>
2020-02-15 13:28:53 +0530

    pushed data to kadfa topic

--- restadapter/src/main/java/com/adapter/restadapter/config/KafkaConfig.java
+++ restadapter/src/main/java/com/adapter/restadapter/config/KafkaConfig.java
@@ -1,4 +1,4 @@
-/*package com.adapter.restadapter.config;
+package com.adapter.restadapter.config;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -7,13 +7,15 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
-public class Configuration {
+@Configuration
+@EnableKafka
+public class KafkaConfig {
 	
 	@Value(value = "${kafka.bootstrapAddress}")
 	private String bootstrapAddress;
@@ -22,6 +24,7 @@ public class Configuration {
 	public ProducerFactory<String, String> producerFactory() {
 		Map<String, Object> configProps = new HashMap<>();
 		configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
+		configProps.put(ProducerConfig.CLIENT_ID_CONFIG, "Adapter1");
 		configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return new DefaultKafkaProducerFactory<>(configProps);
@@ -33,4 +36,3 @@ public class Configuration {
 	}
 
 }
-*/
